3c502daea21421bf0f43fff902dc8da55cb6fa02
Davidda84 <davidda83@educastur.es>
2020-11-30 17:45:28 +0100

    Ejercicio Retratos Robot Resuelto en la version v2.0 de la unidad 4

--- src/retratos/RetratosRobot.java
+++ src/retratos/RetratosRobot.java
@@ -1,12 +1,12 @@
 package retratos;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class RetratosRobot {
 
-	private static String reinicio;
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		/*
 		  Programa para crear retratos robot con cadenas de caracteres. Una vez ejecutado el programa, 
 		  el usuario puede crear tantos retratos como desee antes de finalizar. Los retratos se crean 
@@ -34,174 +34,162 @@ public class RetratosRobot {
 		 |  ---  |
 		 \,,,,,,,/
 		 */
-		int pelo,ojos,nariz,boca,barbilla;
-		String tipoPelo,tipoOjos,tipoNariz,tipoBoca,tipoBarbilla;
-		try (Scanner teclado = new Scanner(System.in)) {
+		int eleccion;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String [] retratoRobot = new String [5];
+		String [] menuOpciones = {"el pelo","los ojos","la nariz","la boca","la barbilla","ver su retrato","reiniciar","salir"};
 			System.out.println("Vamos a crear un retrato Robot");
-			
+			System.out.println("¿Por donde quiere empezar?");
 			do {
-				tipoPelo = null;
-				tipoOjos = null;
-				tipoNariz = null;
-				tipoBoca = null;
-				tipoBarbilla = null;
-				reinicio = null;
-
-					System.out.println("Lo primero será escoger el tipo de pelo, estas son las opciones:");
-					do {
-						System.out.println("1. WWWWWWWWW");
-						System.out.println("2. \\\\\\ /////");
-						System.out.println("3. |\"\"\"\"\"\"\"|");
-						System.out.println("4. |||||||||");
-						pelo=teclado.nextInt();
-						if (pelo < 1 | pelo > 4) {
-							System.out.println("Error!!!!");
-							System.out.println("Elija entre las opciones posibles");
+				for (int i = 0; i < menuOpciones.length; i++) {
+					if (menuOpciones[i] != "ver su retrato" && menuOpciones[i] != "reiniciar" && menuOpciones[i] != "salir") {
+						System.out.println("Pulse " + (i+1) + ", para seleccionar " + menuOpciones[i] + ".");
+					}
+					else {
+						System.out.println("Pulse " + (i+1) + ", para " + menuOpciones[i] + ".");
+					}
+				}
+				eleccion = Integer.parseInt(reader.readLine());
+				switch(eleccion) {
+				case 1:
+					retratoRobot[0] = TipoPelo();
+		 			break;
+		 		case 2:
+		 			retratoRobot[1] = TipoOjos();
+		 			break;
+		 		case 3:
+		 			retratoRobot[2] = TipoNariz();
+		 			break;
+		 		case 4:
+		 			retratoRobot[3] = TipoBoca();
+		 			break;
+				case 5:
+					retratoRobot[4] = TipoBarbilla();
+					break;
+				case 6:
+					boolean donePelo = false;
+					boolean doneOjos = false;
+					boolean doneNariz = false;
+					boolean doneBoca = false;
+					boolean doneBarbilla = false;
+					String [] errorRetrato = new String [5];
+					if (retratoRobot[0] != null) {
+						donePelo = true;
+					}
+					else {
+						errorRetrato[0] = "   - Pelo";
+					}
+					if (retratoRobot[1] != null) {
+						doneOjos = true;
+					}
+					else {
+						errorRetrato[1] = "   - Ojos";
+					}
+					if (retratoRobot[2] != null) {
+						doneNariz = true;
+					}
+					else {
+						errorRetrato[2] = "   - Nariz";
+					}
+					if (retratoRobot[3] != null) {
+						doneBoca = true;
+					}
+					else {
+						errorRetrato[3] = "   - Boca";
+					}
+					if (retratoRobot[4] != null) {
+						doneBarbilla = true;
+					}
+					else {
+						errorRetrato[4] = "   - Barbilla";
+					}
+					if (donePelo && doneOjos && doneNariz && doneBoca && doneBarbilla) {
+						Retrato(retratoRobot);
+					}
+					else {
+						System.out.println("*******************ERROR*******************");
+						System.out.println("Su retrato esta incompleto, revise los siguientes campos: ");
+						for (int i = 0; i < errorRetrato.length; i++) {
+							if (errorRetrato[i] != null) {
+								System.out.println(errorRetrato[i]);								
+							}
 						}
+						System.out.println("*******************ERROR*******************");
 					}
-					while (pelo < 1 | pelo > 4);
-					
-					System.out.println("¿En que tipo de ojos estas pensando?, estas son las opciones:");
-					do {
-						System.out.println("1. | O   O |");
-						System.out.println("2. |-{. .}-|");
-						System.out.println("3. |-{o o}-|");
-						System.out.println("4. | \\   / |");
-						ojos=teclado.nextInt();
-						if (ojos < 1 | ojos > 4) {
-							System.out.println("Error!!!!");
-							System.out.println("Elija entre las opciones posibles");
-						}
-					}
-					while (ojos < 1 | ojos > 4);
-
-					System.out.println("Ahora la nariz y las orejas, estas son las opciones:");
-					do{
-						System.out.println("1. @   J   @");
-						System.out.println("2. {   \"   }");
-						System.out.println("3. [   j   ]");
-						System.out.println("4. <   -   >");
-						nariz=teclado.nextInt();
-						if (nariz < 1 | nariz > 4) {
-							System.out.println("Error!!!!");
-							System.out.println("Elija entre las opciones posibles");
-						}
-					}
-					while (nariz < 1 | nariz > 4);
-
-					System.out.println("La boca, estas son las opciones:");
-					do{
-						System.out.println("1. |  ===  |");
-						System.out.println("2. |   -   |");
-						System.out.println("3. |  ___  |");
-						System.out.println("4. |  ---  |");
-						boca=teclado.nextInt();
-						if (boca < 1 | boca > 4) {
-							System.out.println("Error!!!!");
-							System.out.println("Elija entre las opciones posibles");
-						}
-					}
-					while (boca < 1 | boca > 4);
-					
-					System.out.println("Y por último la barbilla, estas son las opciones:");
-					do{
-						System.out.println("1. \\_______/");
-						System.out.println("2. \\,,,,,,,/");
-						barbilla=teclado.nextInt();
-						if (barbilla < 1 | barbilla > 2) {
-							System.out.println("Error!!!!");
-							System.out.println("Elija entre las opciones posibles");
-						}
-					}
-					while (barbilla < 1 | barbilla > 2);
-					
-					switch(pelo) {
-
-				 		case 1:
-				 			tipoPelo="WWWWWWWWW";
-				 			break;
-				 		case 2:
-				 			tipoPelo="\\\\\\//////";
-				 			break;
-				 		case 3:
-				 			tipoPelo="|\"\"\"\"\"\"\"|";
-				 			break;
-				 		case 4:
-				 			tipoPelo="|||||||||";
-				 			break;
-				 	}
-					switch(ojos) {
-
-			 			case 1:
-			 				tipoOjos="| O   O |";
-			 				break;
-			 			case 2:
-			 				tipoOjos="|-{. .}-|";
-			 				break;
-			 			case 3:
-			 				tipoOjos="|-{o o}-|";
-			 				break;
-			 			case 4:
-			 				tipoOjos="| \\   / |";
-			 				break;
-					}
-					switch(nariz) {
-
-			 			case 1:
-			 				tipoNariz="@   J   @";
-			 				break;
-			 			case 2:
-			 				tipoNariz="{   \"   }";
-			 				break;
-			 			case 3:
-			 				tipoNariz="[   j   ]";
-			 				break;
-			 			case 4:
-			 			tipoNariz="<   -   >";
-			 			break;
-					}
-					switch(boca) {
-
-			 			case 1:
-			 				tipoBoca="|  ===  |";
-			 				break;
-			 			case 2:
-			 				tipoBoca="|   -   |";
-			 				break;
-			 			case 3:
-			 				tipoBoca="|  ___  |";
-			 				break;
-			 			case 4:
-			 				tipoBoca="|  ---  |";
-			 				break;
-					}
-					switch(barbilla) {
-
-			 			case 1:
-			 				tipoBarbilla="\\_______/";
-			 				break;
-			 			case 2:
-			 				tipoBarbilla="\\,,,,,,,/";
-			 				break;
-					}
-					
-					System.out.println("Este es su retrato Robot...");
-					System.out.println(tipoPelo);
-					System.out.println(tipoOjos);
-					System.out.println(tipoNariz);
-					System.out.println(tipoBoca);
-					System.out.println(tipoBarbilla);
-
-					System.out.println("Pulse R para Repetir o cualquier tecla para Salir");
-					reinicio=teclado.next();
-				
-			}
-			while(reinicio.equalsIgnoreCase("R"));
-		}
-		
-		
-		{
+					break;
+				case 7:
+					retratoRobot = new String [5];
+					System.out.println("********************************************");
+					System.out.println("******************REINICIO******************");
+					System.out.println("********************************************");
+					break;
+				}	
+				if (eleccion < 1 || eleccion > 8) {
+					System.out.println("Ciñase a las opciones disponibles");
+				}
+			}			
+			while (eleccion != 8);
 			System.out.println("Hasta la proxima");
+	}
+	static String TipoPelo () throws NumberFormatException, IOException {
+		System.out.println("Elija el tipo de pelo");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String [] pelo = {"WWWWWWWWW", "\\\\\\ /////","|\"\"\"\"\"\"\"|","|||||||||"};
+		for (int i = 0; i < pelo.length; i++) {
+			System.out.println((i+1) + ".    " + pelo[i]);
+		}
+			int nPelo = Integer.parseInt(reader.readLine());
+			String tPelo = pelo[(nPelo-1)];
+			return tPelo;	
+	}
+	static String TipoOjos () throws NumberFormatException, IOException {
+		System.out.println("Elija el tipo de ojos");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String [] ojos = {"| O   O |", "|-{. .}-|", "|-{o o}-|", "| \\   / |"};
+		for (int i = 0; i < ojos.length; i++) {
+			System.out.println((i+1) + ".    " + ojos[i]);
+		}
+			int nOjos = Integer.parseInt(reader.readLine());
+			String tOjos = ojos[(nOjos-1)];
+			return tOjos;
+	}
+	static String TipoNariz () throws NumberFormatException, IOException {
+		System.out.println("Elija el tipo de nariz");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String [] nariz = {"@   J   @", "{   \\\"   }", "[   j   ]", "<   -   >"};
+		for (int i = 0; i < nariz.length; i++) {
+			System.out.println((i+1) + ".    " + nariz[i]);
+		}
+			int nNariz = Integer.parseInt(reader.readLine());
+			String tNariz = nariz[(nNariz-1)];
+			return tNariz;
+	}
+	static String TipoBoca () throws NumberFormatException, IOException {
+		System.out.println("Elija el tipo de boca");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String [] boca = {"|  ===  |", "|   -   |", "|  ___  |", "|  ---  |"};
+		for (int i = 0; i < boca.length; i++) {
+			System.out.println((i+1) + ".    " + boca[i]);
+		}
+			int nBoca = Integer.parseInt(reader.readLine());
+			String tBoca = boca[(nBoca-1)];
+			return tBoca;
+	}
+	static String TipoBarbilla () throws NumberFormatException, IOException {
+		System.out.println("Elija el tipo de barbilla");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String [] barbilla = {"\\_______/", "\\,,,,,,,/"};
+		for (int i = 0; i < barbilla.length; i++) {
+			System.out.println((i+1) + ".    " + barbilla[i]);
+		}
+			int nBarbilla = Integer.parseInt(reader.readLine());
+			//teclado.nextLine();
+			String tBarbilla = barbilla[(nBarbilla-1)];
+			return tBarbilla;
+	}
+	static void Retrato (String [] retratoRobot) {
+		for (int i = 0; i < retratoRobot.length;i++) {
+				System.out.println(retratoRobot[i]);
 		}
 	}
 }
